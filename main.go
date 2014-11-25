4a2a16b1148bc1120ddfe7c5b4fdc2753ca4ef40
SeongJae Park <sj38.park@gmail.com>
2014-11-25 03:16:46 +0000

    implement version checking
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- main.go
+++ main.go
@@ -1,15 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"io"
+	"log"
 	"net/http"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello world")
+const baseCheckURL = "https://github.com/torvalds/linux/releases/tag/"
+const version = "v3.18"
+
+func handleKernelRelease(w http.ResponseWriter, r *http.Request) {
+	checkURL := fmt.Sprintf("%s%s", baseCheckURL, version)
+	if testUrl(checkURL) {
+		io.WriteString(w, fmt.Sprintf("%s Released", version))
+	} else {
+		io.WriteString(w, fmt.Sprintf("%s Not released yet", version))
+	}
+}
+
+func testUrl(url string) bool {
+	r, err := http.Head(url)
+	if err != nil {
+		log.Print(err)
+		return false
+	}
+	return r.StatusCode == http.StatusOK
 }
 
 func main() {
-	http.HandleFunc("/", hello)
+	http.HandleFunc("/", handleKernelRelease)
 	http.ListenAndServe(":8000", nil)
 }
