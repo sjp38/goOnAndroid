3110d4a57139241af1b409d4a0273ac19dfd55db
SeongJae Park <sj38.park@gmail.com>
2014-11-28 19:05:45 +0000

    let it works on Android
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
+	"net"
 	"net/http"
+	"os"
+	"path/filepath"
 )
 
 const baseCheckURL = "https://github.com/torvalds/linux/releases/tag/"
@@ -24,10 +30,55 @@ func handleKernelRelease(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func TLSConfig() (*tls.Config, error) {
+	certDir := "/system/etc/security/cacerts"
+	fi, err := os.Stat(certDir)
+	if err != nil {
+		return nil, err
+	}
+	if !fi.IsDir() {
+		return nil, fmt.Errorf("%q not a dir", certDir)
+	}
+	pool := x509.NewCertPool()
+	cfg := &tls.Config{RootCAs: pool}
+
+	f, err := os.Open(certDir)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+	names, _ := f.Readdirnames(-1)
+	for _, name := range names {
+		pem, err := ioutil.ReadFile(filepath.Join(certDir, name))
+		if err != nil {
+			return nil, err
+		}
+		pool.AppendCertsFromPEM(pem)
+	}
+	return cfg, nil
+}
+
+func androidDial(network, addr string) (net.Conn, error) {
+	c, err := net.Dial(network, "192.30.252.128:443")
+	if err != nil {
+		return nil, err
+	}
+	return c, err
+}
+
 func testUrl(url string) bool {
-	r, err := http.Head(url)
+	tlsConfig, err := TLSConfig()
 	if err != nil {
 		log.Print(err)
+	}
+
+	tr := &http.Transport{
+		Dial:            androidDial,
+		TLSClientConfig: tlsConfig,
+	}
+	client := &http.Client{Transport: tr}
+	r, err := client.Head(url)
+	if err != nil {
 		return false
 	}
 	return r.StatusCode == http.StatusOK
